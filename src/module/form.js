c26e6305f10e14f0785bd345191f7ddd1a2aef5f
Roberto Kennedy <roberto.kpp@gmail.com>
2025-11-12 22:21:39 -0500

    ****

--- src/module/form.js
+++ src/module/form.js
@@ -1,13 +1,19 @@
 import { phoneValidator } from "../utils/phone-validator.js";
+import dayjs from "dayjs";
 
 const form = document.querySelector("form");
 const submit = document.getElementById("btn-submit");
 const tutorsName = document.getElementById("tutors-name");
 const petName = document.getElementById("pet-name-forn");
 const tel = document.getElementById("tel");
+const formDate = document.getElementById("form-date");
+const formHour = document.getElementById("form-hour");
+const formService = document.getElementById("dec-service");
 
 // ativar o validor de numero
 phoneValidator(tel);
+formDate.value = dayjs().format("YYYY-MM-DD");
+formHour.value = dayjs().format("HH:mm");
 
 form.onsubmit = (event) => {
   event.preventDefault();
@@ -24,7 +30,19 @@ form.onsubmit = (event) => {
       alert("Insira o nome do pet!");
     }
 
-    console.log(name, pet, tel.value);
+    const descService = formService.value.trim();
+    if (!descService) {
+      alert("Insira o servico que deseja !!");
+    }
+
+    console.log(
+      name,
+      pet,
+      tel.value,
+      descService,
+      formDate.value,
+      formHour.value
+    );
 
     
   } catch (error) {
